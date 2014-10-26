b9323e9a7ace4384236d0a42382fad61f7b39538
Colin Teal <the30yearswar@hotmail.com>
2014-10-25 21:33:58 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var send = require('./lib/send');
+var render = require('./lib/render');
+var env = require('ffl-utils').environment;
+
+exports.sendInvitation = function (league, recipient, token, callback) {
+  var subject = 'League Invitation';
+
+  var html = render('invitation', {
+    token: token,
+    applicationUrl: env.APPLICATION_URL
+  });
+
+  var sender = {email: 'ffl-noreply@ffl.birdducksoftware.com', name: 'Fantasy Film League'};
+
+  send(subject, sender, recipient, html, callback);
+};
